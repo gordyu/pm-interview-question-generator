a7419ebcd848a65ca59ccff4764427194fcbc84b
Gordon Yu <gyunit@gmail.com>
2020-04-10 22:16:53 -0700

    code cleanup across app.

--- client/src/interviewer/schedule/rCalendar.jsx
+++ client/src/interviewer/schedule/rCalendar.jsx
@@ -4,7 +4,7 @@ import Iframe from 'react-iframe';
 { /* TODO: Enable scheduling on this calendar */ }
 function rCalendar() {
   return (
-      <Iframe url="https://calendar.google.com/calendar/selfsched?sstoken=UUZRckhfdFU4RHJzfGRlZmF1bHR8ZjUxNDVjYzZkMWU3NGE2NzA2ZjZiZjZmY2FmMDVmMTY"
+      <Iframe url="https://calendar.google.com/calendar/embed?src=awip2.0.0%40gmail.com&ctz=America%2FLos_Angeles"
         width="800px"
         height="600px"
         id="calendar"
