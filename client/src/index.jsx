03e77cee2a5f990698c350f972b143d078b786f6
Gordon Yu <gyunit@gmail.com>
2020-04-12 12:49:51 -0700

    installed regenerator-runtime

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,15 +4,28 @@ import ReactDOM from "react-dom";
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = { madlibs: [] }; 
+  }
 
-    }; 
+  async componentDidMount() {
+    const res = await fetch("http://localhost:5000/");
+    this.setState({ madlibs: res });
   }
 
   render() {
     return (
       <div>
-        Hello??
+        <h2>Universal PM Interview Question</h2>
+        <p></p>
+        <h2>Amazon Interview Quesiton</h2>
+        <p></p>
+        <h2>Facebook Interview Question</h2>
+        <p></p>
+        <h2>Microsoft Interview Question</h2>
+        <p></p>
+        <h2>Google Interviews Question</h2>
+        <p></p>
+        <button>Again!</button>
       </div>
     );
   }
